41934c36c3c6b1bd93d19946732bdad0b422d3dd
Jerry Peng <jerry.boyang.peng@gmail.com>
2017-11-13 01:50:25 -0800

    offline with kubernetes 1.7

---
+++ roles/standalone/create-package/download-rpms.sh
@@ -0,0 +1,14 @@
+sudo yum install --downloadonly --installroot=/var/tmp/streamlio --releasever=7 --downloaddir=rpms --nogpgcheck docker-engine-1.13.1
+sudo yum install --downloadonly --installroot=/var/tmp/streamlio --releasever=7 --downloaddir=rpms --nogpgcheck python-docker
+sudo yum install --downloadonly --installroot=/var/tmp/streamlio --releasever=7 --downloaddir=rpms --nogpgcheck libselinux-python
+sudo yum install --downloadonly --installroot=/var/tmp/streamlio --releasever=7 --downloaddir=rpms --nogpgcheck device-mapper
+sudo yum install --downloadonly --installroot=/var/tmp/streamlio --releasever=7 --downloaddir=rpms --nogpgcheck python-httplib2
+sudo yum install --downloadonly --installroot=/var/tmp/streamlio --releasever=7 --downloaddir=rpms --nogpgcheck openssl
+sudo yum install --downloadonly --installroot=/var/tmp/streamlio --releasever=7 --downloaddir=rpms --nogpgcheck curl
+sudo yum install --downloadonly --installroot=/var/tmp/streamlio --releasever=7 --downloaddir=rpms --nogpgcheck rsync
+sudo yum install --downloadonly --installroot=/var/tmp/streamlio --releasever=7 --downloaddir=rpms --nogpgcheck bash-completion
+sudo yum install --downloadonly --installroot=/var/tmp/streamlio --releasever=7 --downloaddir=rpms --nogpgcheck socat
+sudo yum install --downloadonly --installroot=/var/tmp/streamlio --releasever=7 --downloaddir=rpms --nogpgcheck unzip
+sudo yum install --downloadonly --installroot=/var/tmp/streamlio --releasever=7 --downloaddir=rpms --nogpgcheck epel-release
+sudo yum install --downloadonly --installroot=/var/tmp/streamlio --releasever=7 --downloaddir=rpms --nogpgcheck ebtables
+sudo createrepo --database rpms
